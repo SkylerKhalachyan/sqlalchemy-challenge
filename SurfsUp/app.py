3c0b13007dcd0cec541e21539519d8af0960378f
Skyler Khalachyan <skyler.khalachyan@gmail.com>
2023-07-25 10:50:09 -0700

    Completing Jupyter Notebook, Updating app.py

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,30 +1,52 @@
 # Import the dependencies.
-
+from flask import Flask
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func, Text
 
 
 #################################################
 # Database Setup
 #################################################
-
+# create engine to hawaii.sqlite
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
-
+Base = automap_base()
 # reflect the tables
-
+Base.prepare(autoload_with=engine)
 
 # Save references to each table
-
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
-
-
+## Flask constructor takes the name of
+# current module (__name__) as argument.
+app = Flask(__name__)
 
 
 #################################################
 # Flask Routes
 #################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
