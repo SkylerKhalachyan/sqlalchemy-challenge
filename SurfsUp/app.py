591623303e6ea3dac38811394b1a95fcf206741d
Skyler Khalachyan <skyler.khalachyan@gmail.com>
2023-07-25 11:24:34 -0700

    Updating app.py

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -31,11 +31,42 @@ session = Session(engine)
 # current module (__name__) as argument.
 app = Flask(__name__)
 
-
 #################################################
 # Flask Routes
 #################################################
 
+#From Day 3 Activity 4
+# @app.route("/")
+# def home():
+    # print("Server received request for 'Home' page...")
+    # return "Welcome to my 'Home' page!" 
+
+# 1 "/"
+@app.route('/')
+def home():
+    print("Server received request for 'Home' page...")
+    return "Home Page"
+
+#2 "/api/v1.0/precipitation"
+@app.route('/api/v1.0/precipitation')
+def precipitation():
+
+    print("Server received request for 'precipitation' page...")
+    return 
+
+#3 "/api/v1.0/stations"
+@app.route('/api/v1.0/stations')
+def stations():
+
+    print("Server revceived request for 'stations' page...")
+    return
+
+#4 "/api/v1.0/tobs"
+@app.route('/api/v1.0/tobs')
+def tobs():
+
+    print("Server received request for 'tobs' page...")
+    return
 
 
 
@@ -46,7 +77,7 @@ app = Flask(__name__)
 
 
 
-
-
+# Best practice to keep this piece included
 if __name__ == "__main__":
+    # to run Flask app
     app.run(debug=True)
